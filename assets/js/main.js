1831ef1aead174bb83aecbcaabd9562ce3af566a
gbelda <gbelda56@gmail.com>
2021-11-09 15:10:43 +0100

    Created email generator using axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,18 @@
 const app = new Vue({
     el: '#root',
     data: {
-
+        generatedEmail: '',
+        emails: []
+    },
+    mounted() {
+        for (let index = 0; index < 10; index++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    generateEmail = { email: response.data.response }
+                    this.emails.push(generateEmail)
+                })
+        }
     }
+
 })
